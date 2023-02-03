9c00758d15c270d91e916c910794328cd826d161
Drew Anello <drew.anello@gmail.com>
2023-02-03 12:56:57 -0500

    nav routes working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import "../src/App.css"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Home from './pages/Home'
+import Nav from "./components/nav"
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <div>App</div>
-  )
+
+
+function App () {
+return(
+  <div>
+  <Nav />
+    <Routes>  
+    <Route  path="/" element={<Home />} />
+    <Route  path="/about" element={<About />} />
+    <Route  path="/contact" element={<Contact />} />
+</Routes>
+
+</div>
+  
+)
 }
-
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  const img = 'src/images/Badface.png'
+  return (
+    <div className="home">
+      {/* <div className="homeHeader"><img src={img}></img>
+      </div> */}
+<h1>Hey</h1>
+
+        </div>
+        
+  )
+}
+
+export default Home
